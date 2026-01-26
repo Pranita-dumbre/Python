d2ed8b909b740633c0e0eb307100d8efc4355c79
Pranita-dumbre <pranitadumbre1885@gmail.com>
2026-01-26 23:54:07 +0530

    Add files via upload

---
+++ Assignmemt_19/Question19_5.py
@@ -0,0 +1,29 @@
+from functools import reduce 
+
+def main():
+    print("Nymber of elements :")
+    Size = int(input())
+
+    Data = list()
+
+    print("Enter elements :")
+    for i in range(Size):
+        value = int(input())
+        Data.append(value)
+    print("The Actual data is : ",Data)
+
+    Fdata = list(filter(lambda Arr : Arr % 2 != 0 ,Data))
+
+    print(Fdata)
+
+    Mdata = list(map(lambda Brr : Brr * 2,Fdata))
+
+    print(Mdata)
+
+    Rdata = int(reduce(lambda a, b: (a > b) and a or b,Mdata))
+
+    print(Rdata)
+
+
+if __name__ == "__main__":
+    main()
